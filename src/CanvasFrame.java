ec72c9d47bf5edc9c9383728b92d8110f5d48790
Caeden-Mitchell <111471538+Caeden-Mitchell@users.noreply.github.com>
2023-10-05 10:20:37 -0600

    Working one panel

--- src/CanvasFrame.java
+++ src/CanvasFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CanvasFrame extends JFrame {
     CanvasFrame() {
@@ -6,6 +7,7 @@ public class CanvasFrame extends JFrame {
         this.setTitle("Etch-a-Sketch");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.setPreferredSize(new Dimension(500,500));
         this.pack();
         this.setVisible(true);
 
--- src/CanvasPanel.java
+++ src/CanvasPanel.java
@@ -1,7 +1,73 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class CanvasPanel extends JPanel{
+    static final int SCREEN_WIDTH = 1000;
+    static final int SCREEN_HEIGHT = 1000;
+    static final int UNIT_SIZE = 10;
+
+    final int[] x = new int[SCREEN_WIDTH/UNIT_SIZE];
+    final int[] y = new int[SCREEN_HEIGHT/UNIT_SIZE];
+
+    /*
+    R = right
+    L = left
+    U = up
+    D = down
+     */
+    char direction = 'R';
+    
+    boolean running = false;
 
 
+    CanvasPanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        
+        start();
+    }
+    
+    public void start() {
+        running = true;
+        repaint();
+    }
+    
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
 
+    }
+    public void draw(Graphics g) {
+        if (running) {
+            //draws the grid
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+        }
+    }
+    
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    direction = 'L';
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    direction = 'R';
+                    break;
+                case KeyEvent.VK_UP:
+                    direction = 'U';
+                    break;
+                case KeyEvent.VK_DOWN:
+                    direction = 'D';
+                    break;
+            }
+        }
+    }
 }
