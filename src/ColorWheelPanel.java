1c01e782b02533f7acb71a4c21fb6012d4b62215
Caeden-Mitchell <111471538+Caeden-Mitchell@users.noreply.github.com>
2023-10-13 22:13:14 -0600

    still working

--- src/ColorWheelPanel.java
+++ src/ColorWheelPanel.java
@@ -1,35 +1,61 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
-public class ColorWheelPanel extends JPanel{
-    static final int SCREEN_WIDTH = 500;
-    static final int SCREEN_HEIGHT = 500;
-
-    static int WIDTH = 150;
-    static int HEIGHT = 150;
-    static int X = 250-(WIDTH/2);
-    static int Y = 250-(HEIGHT/2);
-
-
-    ColorWheelPanel() {
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-        this.setBackground(Color.GRAY);
-        this.setFocusable(true);
+public class ColorWheelPanel extends JComponent {
+    public ColorWheelPanel() {
+        setPreferredSize(new Dimension(400, 400));
     }
 
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        draw(g);
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        int w = getWidth();
+        int h = getHeight();
+        int margin = 10;
+        int radius = (Math.min(w, h) - 2 * margin) / 2;
+        int cx = w / 2;
+        int cy = h / 2;
+        float[] dist = {0.F, 1.0F};
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, w, h);
+        for (int angle = 0; angle < 360; ++angle) {
+            Color color = hsvToRgb(angle, 1.0, 1.0);
+            Color[] colors = {Color.WHITE, color};
+            RadialGradientPaint paint = new RadialGradientPaint(cx, cy,
+                    radius, dist, colors);
+            g2.setPaint(paint);
+            g2.fillArc(cx - radius, cy - radius, radius * 2, radius * 2,
+                    angle, 1);
+        }
     }
 
-    public void draw(Graphics g) {
-        g.drawLine(0,250,500,250);
-        g.drawLine(250,0,250,500);
-
-
-
-        g.drawOval(X, Y, WIDTH, HEIGHT);
+    public static Color hsvToRgb(int h, double s, double v) {
+        double hp = h/60.0;
+        double chroma = s * v;
+        double x = chroma * (1 - Math.abs(hp % 2.0 - 1));
+        double m = v - chroma;
+        double red = 0, green = 0, blue = 0;
+        if (hp <= 1) {
+            red = chroma;
+            green = x;
+        } else if (hp <= 2) {
+            red = x;
+            green = chroma;
+        } else if (hp <= 3) {
+            green = chroma;
+            blue = x;
+        } else if (hp <= 4) {
+            green = x;
+            blue = chroma;
+        } else if (hp <= 5) {
+            red = x;
+            blue = chroma;
+        } else {
+            red = chroma;
+            blue = x;
+        }
+        red += m;
+        green += m;
+        blue += m;
+        return new Color((int)(red * 255), (int)(green * 255), (int)(blue * 255));
     }
 }
